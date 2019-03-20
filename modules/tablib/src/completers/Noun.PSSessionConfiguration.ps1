7685622cf51074cc30008f9c26d9d8bde5fd3655
Jason Archer <jasonmarcher@gmail.com>
2019-03-19 21:23:48 -0700

    Add PSSessionConfiguration support

---
+++ modules/tablib/src/completers/Noun.PSSessionConfiguration.ps1
@@ -0,0 +1,21 @@
+$Completion_PSSessionConfigurationName = {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+    Get-PSSessionConfiguration "$Argument*" | NewTabItem -Value {$_.Name} -Text {$_.Name} -ResultType ParameterValue
+}
+
+RegisterArgumentCompleter -CommandName "Disable-PSSessionConfiguration" -ParameterName "Name" -ScriptBlock $Completion_PSSessionConfigurationName
+RegisterArgumentCompleter -CommandName "Enable-PSSessionConfiguration" -ParameterName "Name" -ScriptBlock $Completion_PSSessionConfigurationName
+RegisterArgumentCompleter -CommandName "Get-PSSessionConfiguration" -ParameterName "Name" -ScriptBlock $Completion_PSSessionConfigurationName
+RegisterArgumentCompleter -CommandName "Register-PSSessionConfiguration" -ParameterName "Name" -ScriptBlock $Completion_PSSessionConfigurationName
+RegisterArgumentCompleter -CommandName "Receive-PSSessionConfiguration" -ParameterName "Name" -ScriptBlock $Completion_PSSessionConfigurationName
+RegisterArgumentCompleter -CommandName "Set-PSSessionConfiguration" -ParameterName "Name" -ScriptBlock $Completion_PSSessionConfigurationName
+
+$Completion_PSSessionConfigurationConfigurationTypeName = {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+    ## TODO:
+}
+
+RegisterArgumentCompleter -CommandName "Register-PSSessionConfiguration" -ParameterName "ConfigurationTypeName" -ScriptBlock $Completion_PSSessionConfigurationConfigurationTypeName
+RegisterArgumentCompleter -CommandName "Set-PSSessionConfiguration" -ParameterName "ConfigurationTypeName" -ScriptBlock $Completion_PSSessionConfigurationConfigurationTypeName
