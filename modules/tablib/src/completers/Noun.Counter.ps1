ecd35776a50bc6c3e23b60916149442f563e70ef
Jason Archer <jasonmarcher@gmail.com>
2019-03-21 22:05:33 -0700

    Add support for Counters

---
+++ modules/tablib/src/completers/Noun.Counter.ps1
@@ -0,0 +1,30 @@
+$Completion_Counter = {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+    $Parameters = @{}
+
+    Get-Counter -ListSet * @Parameters | Where-Object {$_.PathsWithInstances -like "*$wordToComplete*"} |
+        Sort-Object PathsWithInstances | NewTabItem -Value {$_.PathsWithInstances} -Text {$_.PathsWithInstances} -ResultType ParameterValue
+}
+
+RegisterArgumentCompleter -CommandName "Get-Counter" -ParameterName "Version" -ScriptBlock $Completion_Counter
+RegisterArgumentCompleter -CommandName "Import-Counter" -ParameterName "Version" -ScriptBlock $Completion_Counter
+
+$Completion_CounterSet = {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+    $Parameters = @{}
+
+    Get-Counter -ListSet "$wordToComplete*" @Parameters | NewTabItem -Value {$_.CounterSetName} -Text {$_.CounterSetName} -ResultType ParameterValue
+}
+
+RegisterArgumentCompleter -CommandName "Get-Counter" -ParameterName "Version" -ScriptBlock $Completion_CounterSet
+RegisterArgumentCompleter -CommandName "Import-Counter" -ParameterName "Version" -ScriptBlock $Completion_CounterSet
+
+$Completion_CounterFormat = {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+    "CSV", "TSV", "BLG" | Where-Object {$_ -like "$wordToComplete*"} | NewTabItem -Value {$_} -Text {$_} -ResultType ParameterValue
+}
+
+RegisterArgumentCompleter -CommandName "Export-Counter" -ParameterName "FileFormat" -ScriptBlock $Completion_CounterFormat
