21ef1837824c38b35f08c1587219c1b9e5e85c82
Jason Archer <jasonmarcher@gmail.com>
2019-03-23 13:31:26 -0700

    Add Service support

---
+++ modules/powertabargcompleters/src/completers/Noun.Service.ps1
@@ -0,0 +1,34 @@
+$Completion_ServiceDisplayName = {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+    Get-Service -DisplayName "*$Argument*" | New-TabItem -Value {$_.DisplayName} -Text {$_.DisplayName} -ResultType ParameterValue
+
+    $Parameters = @{}
+
+    Get-Service -DisplayName "*$wordToComplete*" @Parameters |
+        NewTabItem -Value {$_.DisplayName} -Text {$_.DisplayName} -ResultType ParameterValue
+}
+
+RegisterArgumentCompleter -CommandName "Get-Service" -ParameterName "DisplayName" -ScriptBlock $Completion_ServiceDisplayName
+RegisterArgumentCompleter -CommandName "Restart-Service" -ParameterName "DisplayName" -ScriptBlock $Completion_ServiceDisplayName
+RegisterArgumentCompleter -CommandName "Resume-Service" -ParameterName "DisplayName" -ScriptBlock $Completion_ServiceDisplayName
+RegisterArgumentCompleter -CommandName "Start-Service" -ParameterName "DisplayName" -ScriptBlock $Completion_ServiceDisplayName
+RegisterArgumentCompleter -CommandName "Stop-Service" -ParameterName "DisplayName" -ScriptBlock $Completion_ServiceDisplayName
+RegisterArgumentCompleter -CommandName "Suspend-Service" -ParameterName "DisplayName" -ScriptBlock $Completion_ServiceDisplayName
+
+$Completion_ServiceName = {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
+
+    $Parameters = @{}
+
+    Get-Service -Name "$wordToComplete*" @Parameters | NewTabItem -Value {$_.Name} -Text {$_.Name} -ResultType ParameterValue
+}
+
+RegisterArgumentCompleter -CommandName "Get-Service" -ParameterName "Name" -ScriptBlock $Completion_ServiceName
+RegisterArgumentCompleter -CommandName "New-Service" -ParameterName "DependsOn" -ScriptBlock $Completion_ServiceName
+RegisterArgumentCompleter -CommandName "Restart-Service" -ParameterName "Name" -ScriptBlock $Completion_ServiceName
+RegisterArgumentCompleter -CommandName "Resume-Service" -ParameterName "Name" -ScriptBlock $Completion_ServiceName
+RegisterArgumentCompleter -CommandName "Set-Service" -ParameterName "Name" -ScriptBlock $Completion_ServiceName
+RegisterArgumentCompleter -CommandName "Start-Service" -ParameterName "Name" -ScriptBlock $Completion_ServiceName
+RegisterArgumentCompleter -CommandName "Stop-Service" -ParameterName "Name" -ScriptBlock $Completion_ServiceName
+RegisterArgumentCompleter -CommandName "Suspend-Service" -ParameterName "Name" -ScriptBlock $Completion_ServiceName
