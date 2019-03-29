8d458799f98bb854280ec82be179802c7501374e
Jason Archer <jasonmarcher@gmail.com>
2019-03-28 21:09:55 -0700

    Add support for parameters to powershell.exe

--- modules/powertabargcompleters/src/completers/Native.PowerShell.ps1
+++ modules/powertabargcompleters/src/completers/Native.PowerShell.ps1
@@ -1,11 +1,61 @@
 Register-ArgumentCompleter -Native -CommandName powershell -ScriptBlock {
     param($wordToComplete, $commandAst, $cursorPosition)
 
-    $Parameters = "-Command","ConfigurationName","-EncodedCommand","-ExecutionPolicy","-File","-Help",
-    "-InputFormat","-Mta","-NoExit","-NoLogo","-NonInteractive","-NoProfile","-OutputFormat",
-    "-PSConsoleFile","-Sta","-Version","-WindowStyle"
-    $Parameters | Where-Object {$_ -like "$wordToComplete*"} |
-        NewTabItem -Value {$_} -Text {$_} -ResultType wordToComplete
+    $Parameters = '-Command','-ConfigurationName','-EncodedCommand','-ExecutionPolicy','-File','-Help',
+    '-InputFormat','-Mta','-NoExit','-NoLogo','-NonInteractive','-NoProfile','-OutputFormat',
+    '-PSConsoleFile','-Sta','-Version','-WindowStyle'
+
+    $LastArgument = $commandAst.CommandElements[-1]
+
+    if ($LastArgument -is [System.Management.Automation.Language.CommandParameterAst]) {
+        switch ($LastArgument.ParameterName) {
+            'Command' {
+                break
+            }
+            'ConfigurationName' {
+                break
+            }
+            'EncodedCommand' {
+                break
+            }
+            'ExecutionPolicy' {
+                'Unrestricted', 'RemoteSigned', 'AllSigned', 'Restricted', 'Bypass', 'Undefined' |
+                    NewTabItem -Value {$_} -Text {$_} -ResultType ParameterValue
+                break
+            }
+            'File' {
+                break
+            }
+            'InputFormat' {
+                'Text', 'Xml' | NewTabItem -Value {$_} -Text {$_} -ResultType ParameterValue
+                break
+            }
+            'PSConsoleFile' {
+                break
+            }
+            'Version' {
+                '1.0', '2.0', '3.0', '4.0', '5.0', '5.1', '6.0' |
+                    NewTabItem -Value {$_} -Text {$_} -ResultType ParameterValue
+                break
+            }
+            'Version' {
+                '1.0', '2.0', '3.0', '4.0', '5.0', '5.1', '6.0' |
+                    NewTabItem -Value {$_} -Text {$_} -ResultType ParameterValue
+                break
+            }
+            'WindowStyle' {
+                'Normal','Minimized','Maximized','Hidden' |
+                    NewTabItem -Value {$_} -Text {$_} -ResultType ParameterValue
+                break
+            }
+            default {
+                $Parameters | NewTabItem -Value {$_} -Text {$_} -ResultType ParameterValue
+            }
+        }
+    } else {
+        $Parameters | Where-Object {$_ -like "$wordToComplete*"} |
+            NewTabItem -Value {$_} -Text {$_} -ResultType ParameterValue
+    }
 
     <#
     PowerShell[.exe] [-PSConsoleFile <file> | -Version <version>]
