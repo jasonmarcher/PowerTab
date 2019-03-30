5f9cde2c0186883d6f07ec6b97b8c8ff3e11be02
Jason Archer <jasonmarcher@gmail.com>
2019-03-30 15:28:30 -0700

    Add culture unit tests

---
+++ modules/powertabargcompleters/test/Param.Culture.Tests.ps1
@@ -0,0 +1,36 @@
+. "$TestDirectory/Test-Utility.ps1"
+
+. "$SrcDirectory/completers/Param.Culture.ps1"
+
+Describe -Tag "Unit" "Unit-Param.Culture" {
+    Context "Culture" {
+        It "given No Arguments, it should return all Cultures" {
+            $Results = Invoke-Handler $Completion_Locale
+
+            $AllCultures = [System.Globalization.CultureInfo]::GetCultures([System.Globalization.CultureTypes]::InstalledWin32Cultures)
+
+            $Results.Count | Should -Be $AllCultures.Count
+        }
+    }
+
+    Context "GeoId" {
+        It "given No Arguments, it should return all GeoIds" {
+            $Results = Invoke-Handler $Completion_Geoid
+
+            $AllGeoIds = [System.Globalization.CultureInfo]::GetCultures([System.Globalization.CultureTypes]::InstalledWin32Cultures) |
+                ForEach-Object {try{[System.Globalization.RegionInfo]$_.Name}catch{}} | Select-Object -Unique
+
+            $Results.Count | Should -Be $AllGeoIds.Count
+        }
+    }
+
+    Context "Locale" {
+        It "given No Arguments, it should return all Locales" {
+            $Results = Invoke-Handler $Completion_Locale
+
+            $AllLocales = [System.Globalization.CultureInfo]::GetCultures([System.Globalization.CultureTypes]::InstalledWin32Cultures)
+
+            $Results.Count | Should -Be $AllLocales.Count
+        }
+    }
+}
