9c314638be16f9af8cafb2d9b0264267c7ecaef8
Jason Archer <jasonmarcher@gmail.com>
2019-03-30 15:28:57 -0700

    Add Scope unit tests

---
+++ modules/powertabargcompleters/test/Param.Scope.Tests.ps1
@@ -0,0 +1,19 @@
+. "$TestDirectory/Test-Utility.ps1"
+
+. "$SrcDirectory/completers/Param.Scope.ps1"
+
+Describe -Tag "Unit" "Unit-Param.Scope" {
+    Context "ParamScope" {
+        It "given No Arguments, it should return 4 Items" {
+            $Results = Invoke-Handler $Completion_ParamScope
+
+            $Results.Count | Should -Be 4
+        }
+
+        It "given a Scope 'Global', it should return 1 Item" {
+            $Results = Invoke-Handler $Completion_ParamScope -wordToComplete "Global"
+
+            $Results.Count | Should -Be 1
+        }
+    }
+}
