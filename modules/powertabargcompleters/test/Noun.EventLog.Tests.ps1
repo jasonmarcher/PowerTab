131f043ca0b06bf9fa4e19f9bf7fd0264838d207
Jason Archer <jasonmarcher@gmail.com>
2019-03-30 19:19:59 -0700

    Add EventLogName tests

--- modules/powertabargcompleters/test/Noun.EventLog.Tests.ps1
+++ modules/powertabargcompleters/test/Noun.EventLog.Tests.ps1
@@ -16,4 +16,38 @@ Describe -Tag "Unit" "Unit-Noun.EventLog" {
             $Results.Count | Should -Be 3
         }
     }
+
+    Context "EventLogLogName" {
+        $MockObjects = "foo", "bar", "foobar"
+
+        Mock Get-EventLog {
+            param(
+                [Switch]$List,
+                [Switch]$PSProvider
+            )
+
+            $MockObjects
+        }
+
+        It "given No Arguments, it should return All Items" {
+            $Results = Invoke-Handler $Completion_EventLogLogName
+
+            Assert-MockCalled Get-EventLog -Exactly 1 -Scope It
+            $Results.Count | Should -Be $MockObjects.Count
+        }
+
+        It "given a Name 'foo', it should return 2 Items" {
+            $Results = Invoke-Handler $Completion_EventLogLogName -wordToComplete "foo"
+
+            Assert-MockCalled Get-EventLog -Exactly 1 -Scope It
+            $Results.Count | Should -Be 2
+        }
+
+        It "given a Name '*bar', it should return 2 Items" {
+            $Results = Invoke-Handler $Completion_EventLogLogName -wordToComplete "*bar"
+
+            Assert-MockCalled Get-EventLog -Exactly 1 -Scope It
+            $Results.Count | Should -Be 2
+        }
+    }
 }
